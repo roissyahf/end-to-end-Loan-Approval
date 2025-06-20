9fc978ff0cbd6f9930206e3f46e87004d63e59eb
roissyahf <roissyahfk@gmail.com>
2025-06-20 20:18:53 +0700

    fix: docker image ml-model (4)

--- verify-model-registry.py
+++ verify-model-registry.py
@@ -1,5 +1,18 @@
 import mlflow
+import os
+from dotenv import load_dotenv
 
-mlflow.set_tracking_uri('https://dagshub.com/${{ secrets.DAGSHUB_USERNAME }}/Loan_Approval_Model.mlflow')
-mlflow.pyfunc.load_model('models:/revamp-loan-approval-model/Production')
-print('Model loaded successfully.')
+load_dotenv()
+
+mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
+print("Current tracking URI:", mlflow.get_tracking_uri())
+
+client = mlflow.MlflowClient()
+
+# List registered models
+model_target = client.get_registered_model(name="revamp-loan-approval-model")
+print(f"Registered Models: {model_target}")
+
+# Try loading the specific version
+model = mlflow.pyfunc.load_model("models:/revamp-loan-approval-model/Production")
+print("Model loaded successfully.")
